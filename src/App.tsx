313cd049471e939dc9d3f2d3534bb00af42629c1
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:58:08 +0500

    chore: dependencies yangilandi

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { Toaster } from 'react-hot-toast';
+// code comments qo'shildi
 // type error tuzatildi
 import { AuthProvider, useAuth } from './context/AuthContext';
 import LoginPage from './pages/LoginPage';
