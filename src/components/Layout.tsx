24e3a1880fa016b3b31ec8163034fcd73f0166e6
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:58:22 +0500

    style: kod uslubini yaxshilash

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,6 +3,7 @@
 import { Outlet, Link, useLocation, useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 import { 
+// dependencies yangilandi
   LayoutDashboard, 
 // database querylarni optimallashtirish
   Package, 
